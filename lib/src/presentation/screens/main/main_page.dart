11d450a5b0ff1445f58bcc3911e092f868bd444b
Alisheakho <107799922+Alisheakho@users.noreply.github.com>
2025-04-10 03:20:27 +0300

    ..

--- lib/src/presentation/screens/main/main_page.dart
+++ lib/src/presentation/screens/main/main_page.dart
@@ -1,33 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frip_trading/src/presentation/screens/main/categories.dart';
+import 'package:frip_trading/src/presentation/screens/main/my_order.dart';
+// صفحة الأوردر
+// باقي الصفحات...
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  // الصفحات
+  final List<Widget> _pages = [
+    FilterPage(),        // Home
+    MyOrdersPage(),        // Order
+    Placeholder(),       // Cart (بدّلها بصفحتك)
+    Placeholder(),       // Support
+    Placeholder(),       // Profile
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: const FilterPage(),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          selectedItemColor: Colors.teal,
-          unselectedItemColor: Colors.grey,
-          items: [
-            BottomNavigationBarItem(
-                icon: SvgPicture.asset('assets/SVG/home.svg'), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: SvgPicture.asset('assets/SVG/order.svg'), label: 'Cart'),
-            BottomNavigationBarItem(
-                icon: SvgPicture.asset('assets/SVG/cart.svg'),
-                label: 'Support'),
-            BottomNavigationBarItem(
-                icon: SvgPicture.asset('assets/SVG/support.svg'),
-                label: 'Support'),
-            BottomNavigationBarItem(
-                icon: SvgPicture.asset('assets/SVG/profile.svg'),
-                label: 'Profile'),
-          ],
-        ));
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Color(0xFF70b9be),
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/SVG/home.svg', color: _selectedIndex == 0 ? Color(0xFF70b9be) : Colors.grey),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/SVG/order.svg', color: _selectedIndex == 1 ? Color(0xFF70b9be) : Colors.grey),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/SVG/cart.svg', color: _selectedIndex == 2 ? Color(0xFF70b9be): Colors.grey),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/SVG/support.svg', color: _selectedIndex == 3 ? Color(0xFF70b9be) : Colors.grey),
+            label: 'Support',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/SVG/profile.svg', color: _selectedIndex == 4 ? Color(0xFF70b9be) : Colors.grey),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
